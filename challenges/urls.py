86b771efaacbd8810eeee3834c9e9b9fb0b15aff
Jingyu Cao <jingyu.cao@jonasundderwolf.de>
2023-11-19 20:38:20 +0100

    feat: add dynamic view logic

---
+++ challenges/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# This is dummy data
+# urlpatterns = [
+#     path('january', views.january),
+#     path('february', views.february),
+# ]
+
+# with the <>, we don't need to create a new path for each month
+urlpatterns = [path('<int:month>', views.monthly_challenge_by_number)]
+urlpatterns = [path('<str:month>', views.monthly_challenge)]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+# This is dummy data
+# def january(request):
+#     return HttpResponse("Hello, world. This is for January.")
+
+monthly_challenges = {
+    'january': "January - Eat no meat for the entire month!",
+    'february': "February - Walk for at least 20 minutes every day!",
+    'march': "March - Learn Django for at least 20 minutes every day!",
+    'april': "April - Eat meat for the entire month!",
+    'may': "May - Boulder a lot!",
+    'june': "June - Learn Django more!",
+    'july': "July - Eat eggs for the entire month!",
+    'august': "August - Beat 81 sometimes!",
+    'september': "September - Learn a lot of Django!",
+    'october': "October - Eat vegetables for the entire month!",
+    'november': "November - Walk walk walk!",
+    'december': "December - Learn something else!"
+}
+
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    try: 
+       challenge_text = monthly_challenges[month]
+       return HttpResponse(challenge_text)
+    except: 
+       return HttpResponseNotFound('This month is not supported!')
