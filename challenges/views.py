59b39e2509a4d678cf2ffd366477dcff7bcc7ccc
Jingyu Cao <jingyu.cao@jonasundderwolf.de>
2024-05-15 10:42:38 +0200

    feat: add index template to show challenge list

--- challenges/views.py
+++ challenges/views.py
@@ -25,16 +25,8 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        list_items += f"<li><a href='{month}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {'months': months})
 
 
 def monthly_challenge_by_number(request, month):
