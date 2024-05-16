8dd764c74265891aae472f07b771244888aad4cf
Jingyu Cao <jingyu.cao@jonasundderwolf.de>
2024-05-16 11:01:13 +0200

    feat: add a header and includes it from templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # with the <>, we don't need to create a new path for each month
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name='index'),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name='monthly-challenge')
 ]
